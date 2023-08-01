cc40064ad513e6903faac25406f28469a4903b3e
Omer Melih Savas <omermelihsavas@outlook.com>
2023-08-01 04:03:56 +0300

    finish login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <div className="main-view">
-          <Routes>
-            <Route path='/' element={<Home />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
